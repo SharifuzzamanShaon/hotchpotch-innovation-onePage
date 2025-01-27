f77c547d579e8c760257168f38acac0b9839cf7e
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-27 17:31:56 +0600

    results of ai section

---
+++ src/components/AiResult/AiResultMainSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AIContent from './AiResultSection'
+import AIContentTwo from './AiResultSectionTwo'
+
+const AiResultMainSection = () => {
+  return (
+   <>
+   <AIContent/>
+   <AIContentTwo/>
+   </>
+  )
+}
+
+export default AiResultMainSection
