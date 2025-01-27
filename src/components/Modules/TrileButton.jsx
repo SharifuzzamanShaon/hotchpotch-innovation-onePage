f172671845ba4833faae57e94df3278198b3b0e3
sharifuzzamanShaon <shasan2200@gmail.com>
2025-01-27 17:33:02 +0600

    common btn

---
+++ src/components/Modules/TrileButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TrileButton = () => {
+  return (
+    <button
+      className="px-8 py-3 bg-gradient-to-r text-white text-lg rounded-md"
+      style={{
+        background: "linear-gradient(94.25deg, #2B59FF 0%, #BB2BFF 100%)",
+      }}
+    >
+      Start 14 Days Free Trial
+    </button>
+  );
+};
+
+export default TrileButton;
